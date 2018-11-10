59db41c9bbded0b490826439118ad0269cba8f98
Kazuya Izumi <shikibu9419@gmail.com>
2018-11-10 21:56:32 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    loop {
+        // mut: mutable
+        // 推奨される方法の方が短く書けるように
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+
+        let guess: u32 =
+            match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
